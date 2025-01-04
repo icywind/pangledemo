5aea9cd9de05f7bc0d29c0501817f41de1e6208d
Rick <rcsw.devel@gmail.com>
2025-01-03 16:34:19 -0800

    Initial Commit

---
+++ PangleDemo/PangleDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PangleDemoApp.swift
+//  PangleDemo
+//
+//  Created by Rick Cheng on 1/3/25.
+//
+
+import SwiftUI
+
+@main
+struct PangleDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PangleDemoTests/PangleDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  PangleDemoTests.swift
+//  PangleDemoTests
+//
+//  Created by Rick Cheng on 1/3/25.
+//
+
+import Testing
+@testable import PangleDemo
+
+struct PangleDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
